a273830f57fef9c612fd4a69ad5ef2da4ec03aea
40156896 <michaelkielt@gmail.com>
2024-02-06 14:58:34 +0000

    Adding logic to the ger_user_input function

--- expenses_tracker.py
+++ expenses_tracker.py
@@ -2,18 +2,41 @@ def main():
     # Get user input for specific expense
     get_user_input()
     # Write user expense to a file
-
+    
     # Read file and break down expenses
+    
     pass 
 
 def get_user_input():
-    print("checking this works")
-    pass
+    expense_name = input("Enter the name of the expense here: ")
+    expense_amount = float(input("Enter the amount of the expense in £: "))
+    expense_types = [
+        "Food", 
+        "Rent", 
+        "Work", 
+        "Fun", 
+        "Misc"
+    ]
+
+    while True:
+        print("Please select a category: ")
+        for i, type in enumerate(expense_types):
+            print(f" {i+1}. {type} ")
+
+        value_range = f"1 - {len(expense_types)}"
+        selected_index = int(input(f"Select a number from {value_range}: ")) - 1
+
+        if selected_index in range(len(expense_types)):
+            break
+        else:
+            print("Invalid index selection, please try again.")
 
 def write_expense_to_file():
+    print("checking 2 works")
     pass
 
 def summarise_expenses():
+    print("checking 3 works")
     pass
 
 if __name__ == '__main__':
