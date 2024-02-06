94b8db16295ed004ba8d55bd5b68c71b0804124e
40156896 <michaelkielt@gmail.com>
2024-02-06 14:19:20 +0000

    Adds function names to expenses_tracker

--- expenses_tracker.py
+++ expenses_tracker.py
@@ -1,10 +1,20 @@
 def main():
     # Get user input for specific expense
-
+    get_user_input()
     # Write user expense to a file
 
     # Read file and break down expenses
     pass 
+
+def get_user_input():
+    print("checking this works")
+    pass
+
+def write_expense_to_file():
+    pass
+
+def summarise_expenses():
+    pass
 
 if __name__ == '__main__':
     main()
