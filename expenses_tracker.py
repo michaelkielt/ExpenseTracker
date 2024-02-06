bef67946935a8a94fa0b9c4f8a89f7f19dfdb957
40156896 <michaelkielt@gmail.com>
2024-02-06 14:13:34 +0000

    Adds expenses_tracker.py file

---
+++ expenses_tracker.py
@@ -0,0 +1,10 @@
+def main():
+    # Get user input for specific expense
+
+    # Write user expense to a file
+
+    # Read file and break down expenses
+    pass 
+
+if __name__ == '__main__':
+    main()
