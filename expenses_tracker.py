ebae51b25903cd5c66a00c13dfc023ca6c5bd7e6
40156896 <michaelkielt@gmail.com>
2024-02-07 19:25:04 +0000

    Started implementing the summarise_expenses function

--- expenses_tracker.py
+++ expenses_tracker.py
@@ -5,13 +5,13 @@ def main():
     print("Running the expense tracker!")
     expense_file_path = "expenses.csv"
     # Get user input for specific expense
-    expense = get_user_input()
+    #expense = get_user_input()
     # Write user expense to a file
-    write_expense_to_file(expense, expense_file_path)
+    #write_expense_to_file(expense, expense_file_path)
     
     # Read file and break down expenses
-    
-    pass 
+    summarise_expenses(expense_file_path)
+     
 
 def get_user_input():
     expense_name = input("Enter the name of the expense here: ")
@@ -43,9 +43,19 @@ def write_expense_to_file(expense: Expense, expense_file_path):
     with open(expense_file_path, "a") as file:
         file.write(f"{expense.name}, {expense.type}, {expense.amount} \n")
 
-def summarise_expenses():
-    print("checking 3 works")
-    pass
+def summarise_expenses(expense_file_path):
+    print("Summarising user expenses")
+    expenses = []
+    with open(expense_file_path, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            expense_name, expense_type, expense_amount = line.strip().split(",")
+            line_expense = Expense(
+                name=expense_name,  
+                type=expense_type,
+                amount=float(expense_amount)
+            )
+            expenses.append(line_expense)
 
 if __name__ == '__main__':
     main()
