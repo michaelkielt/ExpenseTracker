d79e25c6a3071a5ad21bebdeff45f5a8b579a7e7
40156896 <michaelkielt@gmail.com>
2024-02-08 17:54:35 +0000

    Adds docstrings to expenses.py

--- expense.py
+++ expense.py
@@ -1,9 +1,30 @@
 class Expense:
+    """
+    Class representing an expense.
+
+    Attributes:
+        name (str): The name of the expense.
+        type (str): The type or category of the expense.
+        amount (float): The amount of money spent on the expense.
+    """
     
     def __init__(self, name, type, amount):
         self.name = name
         self.type = type
         self.amount = amount
+    """
+        Initialises an Expense object.
 
+        Args:
+            name (str): The name of the expense.
+            type (str): The type or category of the expense.
+            amount (float): The amount of money spent on the expense.
+        """
     def __repr__(self) -> str:
         return f"Expense: {self.name}, {self.type}, £{self.amount}"
+    """
+        Returns a string representation of the Expense object.
+
+        Returns:
+            str: A string representation of the Expense object.
+    """
